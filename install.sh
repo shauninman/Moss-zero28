d7a5c82f2012e15c603124cba551c0e66d611b92
Shaun Inman <shaun@panic.com>
2025-01-11 12:30:45 -0500

    updated notes + added tina config to img

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ cp etc/rc.local /root/lichee/target/allwinner/a133-aw3/base-files/etc/
 cp etc/banner /root/lichee/package/base-files/files/etc/
 
 cp -r usr /root/lichee/package/add-rootfs-demo/
+gzip -c /root/lichee/.config > /root/lichee/package/add-rootfs-demo/usr/magicx/tina_config.gz
+
 echo "after building run add-rootfs-demo"
